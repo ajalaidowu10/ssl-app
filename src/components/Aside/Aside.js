1cac47dcf0bbe6a308165a96b8b51167f0df8459
Ajala Idowu <ajalaidowu10@gmail.com>
2022-08-23 03:53:59 +0100

    Add components and style

---
+++ src/components/Aside/Aside.js
@@ -0,0 +1,27 @@
+import react from 'react';
+import './Aside.css'
+
+const Aside = () => (
+	<div className="aside">
+		<div className="heading">
+		 	<h4>CloudletHR</h4>
+		 	<p>
+		 		Industry tested no1 software solutions for modern human resource
+		 		providers over the world
+		 	</p>
+		</div>
+	 	<div className="pay-section">
+	 		<h4>Paying</h4>
+	 		<h3>N 40000 NGN</h3>
+	 		<p>for 300 employment</p>
+	 	</div>
+	 	<div className="make-payment">
+	 		<div className="triangle">
+	 			<div className="triangle-base">Make Payment</div>
+	 		</div>
+	 	</div>
+
+  </div>
+	);
+
+export default Aside
